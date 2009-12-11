a369d07b57b018db07396dc4dfb03262084b4b85
Steffen Buehl <sb@sbuehl.com>
2009-12-11 15:31:52 +0000

    - nicht funktionsfaehig / RMI

---
+++ G2_GUI/src/de/adv/atech/roboter/gui/core/GUIController.java
@@ -0,0 +1,52 @@
+/*
+ * Created on 11.12.2009
+ */
+package de.adv.atech.roboter.gui.core;
+
+import de.adv.atech.roboter.commons.ClientManager;
+
+/**
+ * 
+ * @author sbu
+ * 
+ */
+public class GUIController
+{
+  private ClientManager clientManager;
+
+  /**
+   * 
+   */
+  private static GUIController _instance;
+
+  /**
+   * 
+   * @return
+   */
+  public static GUIController getInstance()
+  {
+    if (GUIController._instance == null)
+    {
+      GUIController._instance = new GUIController();
+    }
+
+    return GUIController._instance;
+  }
+
+  /**
+   * @param clientManager the clientManager to set
+   */
+  public void setClientManager(ClientManager clientManager)
+  {
+    this.clientManager = clientManager;
+  }
+
+  /**
+   * @return the clientManager
+   */
+  public ClientManager getClientManager()
+  {
+    return this.clientManager;
+  }
+
+}
--- G2_GUI/src/de/adv/atech/roboter/gui/rmi/RMIServer.java
+++ G2_GUI/src/de/adv/atech/roboter/gui/rmi/RMIServer.java
@@ -1,72 +1,62 @@
 package de.adv.atech.roboter.gui.rmi;
 
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
 
-import de.adv.atech.roboter.commons.Constant;
+import de.adv.atech.roboter.commons.Client;
 import de.adv.atech.roboter.commons.interfaces.Command;
-import de.adv.atech.roboter.commons.rmi.Discovery;
-import de.adv.atech.roboter.commons.rmi.RMILookup;
 import de.adv.atech.roboter.commons.rmi.ServerInterface;
+import de.adv.atech.roboter.gui.core.GUIController;
 
-public class RMIServer extends UnicastRemoteObject implements ServerInterface {
+/**
+ * 
+ * @author sbu
+ * 
+ */
+public class RMIServer extends UnicastRemoteObject implements ServerInterface
+{
 
-	RMIServer() throws RemoteException {
-		super();
+  RMIServer() throws RemoteException
+  {
+    super();
+  }
 
-		try {
-			LocateRegistry.createRegistry(10232);
-		}
+  /**
+   * 
+   */
+  public void processCommand(List<Command> commandList) throws RemoteException
+  {
 
-		catch (RemoteException ex) {
-			System.out.println("EX: " + ex.getMessage());
-		}
-		try {
-			Properties tmpProps = new Properties();
+    System.out.println("[GUI] Empfangene Daten: ");
 
-			tmpProps.put("jip.rmi.multicast.address", "228.5.6.7");
-			tmpProps.put("jip.rmi.multicast.port", "6789");
-			tmpProps.put("jip.rmi.unicast.port", new Integer(5000).toString());
-			tmpProps.put("jip.rmi.unicast.portRange", "20");
-			tmpProps.put("jip.rmi.protocol.header", "ROBOTHEAD");
-			tmpProps.put("jip.rmi.protocol.delim", "|");
+    for (Iterator<Command> it = commandList.iterator(); it.hasNext();)
+    {
+      Command tmpCommand = it.next();
 
-			Discovery.setProperties(tmpProps);
+      System.out.println(tmpCommand.toString());
+    }
+  }
 
-			// Naming.rebind(Constant.RMI_SERVER_GUI, new RMIServer());
+  /**
+   * 
+   * @param client
+   */
+  public void initConnection(Client client)
+  {
+    // Handshake
+    GUIController.getInstance().getClientManager().registerClient(client);
+  }
 
-			RMILookup.bind(this, Constant.RMI_SERVER_GUI);
+  /**
+ * 
+ */
+  public boolean ping()
+  {
+    System.out.println("** PING? PONG! **");
 
-			System.out.println("Server gestartet");
-		}
-		catch (Exception ex) {
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	public static void main(String[] args) {
-		try {
-			new RMIServer();
-		}
-		catch (Exception ex) {
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	public void processCommand(List<Command> commandList)
-			throws RemoteException {
-
-		System.out.println("Empfangene Daten: ");
-
-		for (Iterator<Command> it = commandList.iterator(); it.hasNext();) {
-			Command tmpCommand = it.next();
-
-			System.out.println(tmpCommand.toString());
-		}
-	}
+    return true;
+  }
 
 }
