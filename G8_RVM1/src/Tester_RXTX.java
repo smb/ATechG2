1af6556e4354017ad269eef747de802585a8cfd0
Nico Sukowski <Nico.Sukowski@googlemail.com>
2010-01-15 11:33:48 +0000

    Funktionierende Testklasse für Ansteuerung

---
+++ G8_RVM1/src/Tester_RXTX.java
@@ -0,0 +1,134 @@
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Tester_RXTX
+{
+    public Tester_RXTX()
+    {
+        super();
+    }
+    
+    void connect ( String portName ) throws Exception
+    {
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if ( portIdentifier.isCurrentlyOwned() )
+        {
+            System.out.println("Error: Port is currently in use");
+        }
+        else
+        {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+            
+            if ( commPort instanceof SerialPort )
+            {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(9600,SerialPort.DATABITS_7,SerialPort.STOPBITS_2,SerialPort.PARITY_EVEN);
+                
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+                
+                (new Thread(new SerialReader(in))).start();
+                (new Thread(new SerialWriter(out))).start();
+
+            }
+            else
+            {
+                System.out.println("Error: Only serial ports are handled by this example.");
+            }
+        }     
+    }
+    
+    /** */
+    public static class SerialReader implements Runnable 
+    {
+        InputStream in;
+        
+        public SerialReader ( InputStream in )
+        {
+            this.in = in;
+        }
+        
+        public void run ()
+        {
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            try
+            {
+                while ( ( len = this.in.read(buffer)) > -1 )
+                {
+                    if(len > 0)
+                    {
+                        System.out.print(new String(buffer,0,len));
+                    }
+                }
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }            
+        }
+    }
+
+    /** */
+    public static class SerialWriter implements Runnable 
+    {
+        OutputStream out;
+        
+        public SerialWriter ( OutputStream out )
+        {
+            this.out = out;
+        }
+        
+        public void run ()
+        {
+            try
+            {                
+                int c = 0;
+//                while ( ( c = System.in.read()) > -1 )
+                while( true )
+                {
+                    String comand = "gc\r";
+                    byte[] coman_arr = comand.getBytes("ASCII");
+                    for(int i = 0; i < coman_arr.length; i++)
+                    {
+                        this.out.write(coman_arr[i]);
+                    }
+                    comand = "go\r";
+                    coman_arr = comand.getBytes("ASCII");
+                    for(int i = 0; i < coman_arr.length; i++)
+                    {
+                        this.out.write(coman_arr[i]);
+                    }
+//                    comand = "gc\r";
+//                    coman_arr = comand.getBytes("ASCII");
+//                    for(int i = 0; i < coman_arr.length; i++)
+//                    {
+//                        this.out.write(coman_arr[i]);
+//                    }
+                }                
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }            
+        }
+    }
+    
+    public static void main ( String[] args )
+    {
+        try
+        {
+            (new Tester_RXTX()).connect("COM1");
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
