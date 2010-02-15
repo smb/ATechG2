e64c8e9f464d0b224fda394038023544853f2fb4
Steffen Buehl <sb@sbuehl.com>
2010-02-15 20:22:25 +0000

    Nest + Sleep registrieren beim connect

--- G8_RVM1/src/de/adv/atech/roboter/rvm1/rmi/RMIClient.java
+++ G8_RVM1/src/de/adv/atech/roboter/rvm1/rmi/RMIClient.java
@@ -3,6 +3,8 @@ package de.adv.atech.roboter.rvm1.rmi;
 import de.adv.atech.roboter.commons.Constant;
 import de.adv.atech.roboter.commons.GenericController;
 import de.adv.atech.roboter.commons.commands.DefaultMove;
+import de.adv.atech.roboter.commons.commands.DefaultNest;
+import de.adv.atech.roboter.commons.commands.DefaultSleep;
 import de.adv.atech.roboter.commons.interfaces.Client;
 import de.adv.atech.roboter.commons.network.NetworkSlaveManager;
 import de.adv.atech.roboter.commons.rmi.ServerInterface;
@@ -15,6 +17,10 @@ public class RMIClient {
 					.getClientManager().getClient(Constant.CLIENT_SELF);
 
 			localClient.getCommandManager().registerCommand(DefaultMove.class);
+
+			localClient.getCommandManager().registerCommand(DefaultSleep.class);
+
+			localClient.getCommandManager().registerCommand(DefaultNest.class);
 
 			NetworkSlaveManager ncl = new NetworkSlaveManager(new RMIServer(),
 					"RVM1");
