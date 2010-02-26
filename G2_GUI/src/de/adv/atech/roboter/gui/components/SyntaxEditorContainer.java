ffcb655c7d58897536ec1451b12dfc9d3daac757
aemik <aemik84@googlemail.com>
2010-02-26 10:13:39 +0000

    properties für SyntaxEditor

--- G2_GUI/src/de/adv/atech/roboter/gui/components/SyntaxEditorContainer.java
+++ G2_GUI/src/de/adv/atech/roboter/gui/components/SyntaxEditorContainer.java
@@ -1,12 +1,15 @@
 package de.adv.atech.roboter.gui.components;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
-import javax.swing.text.EditorKit;
 
 import jsyntaxpane.DefaultSyntaxKit;
 
@@ -18,20 +21,35 @@ public class SyntaxEditorContainer extends Container {
 		this.setLayout(new BorderLayout());
 
 		DefaultSyntaxKit.initKit();
-
+		
 		syntaxEditor = new JEditorPane();
 		JScrollPane scrollPane = new JScrollPane(syntaxEditor);
 		JToolBar toolBar = new JToolBar();
-
+		
 		syntaxEditor.setContentType("text/java");
-
 		toolBar.removeAll();
-		EditorKit kit = syntaxEditor.getEditorKit();
+		DefaultSyntaxKit kit = (DefaultSyntaxKit) syntaxEditor.getEditorKit();
+		
+		InputStream inputSream = SyntaxEditorContainer.class.getResourceAsStream("editor.properties");
+		Properties editorProperties = new Properties();
+		try {
+			editorProperties.load(inputSream);
+			inputSream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		kit.setConfig(editorProperties);		
+		syntaxEditor.setEditorKit(kit);
+		syntaxEditor.setForeground(Color.YELLOW);
+		syntaxEditor.setBackground(Color.BLACK);
+		
 		if (kit instanceof DefaultSyntaxKit) {
 			DefaultSyntaxKit defaultSyntaxKit = (DefaultSyntaxKit) kit;
 			defaultSyntaxKit.addToolBarActions(syntaxEditor, toolBar);
 		}
 		toolBar.validate();
+
 
 		this.add(toolBar, BorderLayout.NORTH);
 		this.add(scrollPane, BorderLayout.CENTER);
